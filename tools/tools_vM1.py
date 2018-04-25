9dc4bfd28622b3203982a934661e11de1449f347
miller <millermrosek@gmail.com>
2018-04-24 23:18:00 -0400

    Conv Encoder and MMNet

--- tools/tools_vM1.py
+++ tools/tools_vM1.py
@@ -1,10 +1,11 @@
 """
     Various methods are kept here to keep the other code files simple
 """
-import torch
-from models import models_mmnet as models
+from models import models_conv_enc as models
 
-def pick_model(args, dicts, num_struc_feats):
+print("New Model")
+
+def pick_model(args, dicts):
     """
         Use args to initialize the appropriate model
     """
@@ -18,35 +19,13 @@ def pick_model(args, dicts, num_struc_feats):
     else:
         bce_weights = None
         
-    if args.struc_dropout_list:
-        dropouts = [float(size) for size in args.struc_dropout_list.split(",")]
-    else:
-        dropouts = []
-                
-    struc_layers = [int(size) for size in args.struc_layer_size_list.split(",")]
-    
-    if args.post_merge_layer_size_list:
-        post_merge_layers = [int(size) for size in args.post_merge_layer_size_list.split(",")]
-    else:
-        post_merge_layers = []
-        
     print("\nBCE weights: " + str(bce_weights))
     
     if args.model == "conv_encoder":
                 
         model = models.ConvEncoder(args.embed_file, kernel_sizes, args.num_filter_maps, args.gpu, dicts, args.embed_size, args.fc_dropout_p, 
-                                   args.conv_activation, bce_weights, args.embed_dropout_bool, args.embed_dropout_p, args.loss, args.post_conv_fc_bool)
-    
-    elif args.model == "mmnet":
+                                   args.conv_activation, bce_weights, args.embed_dropout_bool, args.embed_dropout_p, args.loss)
         
-        model = models.MMNet(args.embed_file, kernel_sizes, args.num_filter_maps, args.gpu, dicts, args.embed_size, 
-                             args.fc_dropout_p, args.conv_activation, bce_weights, args.embed_dropout_bool, 
-                             args.embed_dropout_p, args.loss, num_struc_feats, struc_layers, dropouts, args.struc_activation, 
-                             post_merge_layers, args.post_conv_fc_bool, args.post_conv_fc_dim, args.batch_norm_bool)
-        
-    elif args.model == "saved":
-        model = torch.load(args.test_model)    
-    
     print("\nGPU: " + str(args.gpu))
 
     if args.gpu:
